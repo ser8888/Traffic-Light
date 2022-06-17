0f9fc9319c21bcb958fe3b4393c6b9637e68e026
Sergejs Tiselskis <slokas85@gmail.con>
2022-06-18 00:15:24 +0300

    Standard Type Traffic Light

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,23 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var lightRed: UIView!
-    @IBOutlet var lightYellow: UIView!
-    @IBOutlet var lightGreen: UIView!
+    @IBOutlet var lightRedLabel: UIView!
+    @IBOutlet var lightYellowLabel: UIView!
+    @IBOutlet var lightGreenLabel: UIView!
     @IBOutlet var buttonMain: UIButton!
     var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lightRed.isHidden = false
-        lightRed.alpha = 0.3
-        lightRed.layer.cornerRadius = 50
-        lightYellow.isHidden = false
-        lightYellow.alpha = 0.3
-        lightYellow.layer.cornerRadius = 50
-        lightGreen.isHidden = false
-        lightGreen.alpha = 0.3
-        lightGreen.layer.cornerRadius = 50
+        lightRedLabel.isHidden = false
+        lightRedLabel.alpha = 0.3
+        lightRedLabel.layer.cornerRadius = 50
+        lightYellowLabel.isHidden = false
+        lightYellowLabel.alpha = 0.3
+        lightYellowLabel.layer.cornerRadius = 50
+        lightGreenLabel.isHidden = false
+        lightGreenLabel.alpha = 0.3
+        lightGreenLabel.layer.cornerRadius = 50
         buttonMain.layer.cornerRadius = 15
         buttonMain.configuration = setupButtonMainPressed(with: "S T A R T")
         
@@ -34,19 +34,19 @@ class ViewController: UIViewController {
     @IBAction func buttonMainPressed() {
         switch index {
         case 0 :
-            lightRed.alpha = 1
+            lightRedLabel.alpha = 1
             index += 1
             buttonMain.configuration = setupButtonMainPressed(with: "N E X T")
         case 1 :
-            lightRed.alpha = 0.3
-            lightYellow.alpha = 1
+            lightRedLabel.alpha = 0.3
+            lightYellowLabel.alpha = 1
             index += 1
         case 2 :
-            lightYellow.alpha = 0.3
-            lightGreen.alpha = 1
+            lightYellowLabel.alpha = 0.3
+            lightGreenLabel.alpha = 1
             index += 1
         case 3:
-            lightGreen.alpha = 0.3
+            lightGreenLabel.alpha = 0.3
             buttonMain.configuration = setupButtonMainPressed(with: "S T A R T")
             index = 0
         default :
